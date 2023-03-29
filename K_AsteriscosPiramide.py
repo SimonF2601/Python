e9b94e4df5c0a4626cb98db74b5cf0725ccf9ca2
SimonF2601 <117550387+SimonF2601@users.noreply.github.com>
2023-03-28 20:28:29 -0500

    Update K_AsteriscosPiramide.py

--- K_AsteriscosPiramide.py
+++ K_AsteriscosPiramide.py
@@ -1,1 +1,23 @@
+n = 1 
+m = 0 
 
+while n >= m:
+    a = int(input("Ingresar numero de asteriscos: "))
+    if a % 2 != 0:
+        while n <= a:
+            while n > m:
+                m += 1
+                print('*', end='')
+            print(' ')
+            n += 2
+            m = 0
+        n -= 4
+        while n >= 1:
+            while n > m:
+                print('*', end='')
+                m += 1
+            print(' ')
+            n -= 2
+            m = 0
+    else:
+        print('Ingrese un numero impar')
